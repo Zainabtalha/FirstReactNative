2e40c1dc703f228f870b416f268e8c02800486e1
Zainab Talha <133660467+Zainabtalha@users.noreply.github.com>
2024-09-28 03:13:38 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native"
+
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{color:'red',alignContent:'center'}}>Hello world
+        I am Zainab Talha
+      </Text>
+    </View>
+  )
+}
+
+export default App
